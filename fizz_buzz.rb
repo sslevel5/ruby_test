1623a49bb950af91342f8a7ca5872098da5f0c8f
sslevel5 <sslevel5@gmail.com>
2024-05-11 17:20:17 +0900

    3と5

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,6 +1,15 @@
 def fizz_buzz(n)
-  n.to_s
+  if n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
 end
 
 puts fizz_buzz(2)
 puts fizz_buzz(3)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(10)

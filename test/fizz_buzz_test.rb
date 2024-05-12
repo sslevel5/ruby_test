7037bc12b102f9f7d9267ba00cee6c257a7f2121
sslevel5 <sslevel5@gmail.com>
2024-05-12 21:45:55 +0900

    3.2.2

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require_relative '../lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
@@ -9,6 +10,6 @@ class FizzBuzzTest < Minitest::Test
     assert_equal 'Buzz', fizz_buzz(5)
     assert_equal 'Fizz', fizz_buzz(6)
     assert_equal 'Fizz_Buzz', fizz_buzz(15)
-    assert_equal 'Fizz_Buzz', fizz_buzz(16)
+    # assert_equal 'Fizz_Buzz', fizz_buzz(16)
   end
 end

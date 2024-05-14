dbc28c69d215d75ee5d3767986ad69f90217fbd5
sslevel5 <sslevel5@gmail.com>
2024-05-14 15:13:54 +0900

    リファクタリング_map

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -19,9 +19,13 @@ def to_ints(hex)
   r = hex[1..2]
   g = hex[3..4]
   b = hex[5..6]
-  ints = []
-  [r, g, b].each do |s|
-    ints << s.hex
+  # ints = []
+  # [r, g, b].each do |s|
+  #   ints << s.hex
+  # end
+  # ints
+
+  [r, g, b].map do |s|
+    s.hex
   end
-  ints
 end

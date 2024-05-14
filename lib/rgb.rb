6601df594fcd35214e1c539b55b2567d354195e9
sslevel5 <sslevel5@gmail.com>
2024-05-14 14:47:27 +0900

    リファクタリング_sum

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -4,9 +4,13 @@ def to_hex(r, g, b)
   # g.to_s(16).rjust(2, '0') +
   # b.to_s(16).rjust(2, '0')
 
-  hex = '#'
-  [r, g, b].each do |n|
-    hex += n.to_s(16).rjust(2, '0')
+  # hex = '#'
+  # [r, g, b].each do |n|
+  #   hex += n.to_s(16).rjust(2, '0')
+  # end
+  # hex
+
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
   end
-  hex
 end

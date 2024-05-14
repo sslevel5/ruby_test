69735ee9372d99e945db47380709f5c9b0fe98f0
sslevel5 <sslevel5@gmail.com>
2024-05-14 15:23:52 +0900

    リファクタリング応用

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -16,16 +16,18 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
-  r = hex[1..2]
-  g = hex[3..4]
-  b = hex[5..6]
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
   # ints = []
   # [r, g, b].each do |s|
   #   ints << s.hex
   # end
   # ints
 
-  [r, g, b].map do |s|
-    s.hex
-  end
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+
+  hex.scan(/\w\w/).map(& :hex)
 end

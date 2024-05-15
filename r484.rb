0935ec8ac0357ba8868035623f8d6977540c7a8e
sslevel5 <sslevel5@gmail.com>
2024-05-15 13:52:45 +0900

    483

---
+++ r484.rb
@@ -0,0 +1,15 @@
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+
+areas = []
+
+dimensions.each do |dimension|
+  length = dimension[0]
+  width = dimension[1]
+  areas << length * width
+end
+
+puts areas

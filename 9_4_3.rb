cc1670847784e79a8b883c111dca0694d9836fc4
sslevel5 <sslevel5@gmail.com>
2024-05-22 14:47:40 +0900

    9.4.3

---
+++ 9_4_3.rb
@@ -0,0 +1,15 @@
+require 'date'
+
+def convert_reiwa_to_date(reiwa_text)
+  begin
+    m = reiwa_text.match(/令和(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+    year = m[:jp_year].to_i + 2018
+    month = m[:month].to_i
+    day = m[:day].to_i
+    Date.new(year, month, day)
+  rescue
+    nil
+  end
+end
+
+puts convert_reiwa_to_date(令和3年12月31日)

fc010e9000264da601c89c1be85d3923e9a4c2fd
sslevel5 <sslevel5@gmail.com>
2024-05-23 19:25:54 +0900

    10.4.6

--- lib/effects.rb
+++ lib/effects.rb
@@ -5,9 +5,15 @@ module Effects
     end
   end
 
-  def self.echo
+  def self.echo(rate)
     ->(words) do
       words.each_char.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
+
+  def self.loud(level)
+    ->(word) do
+      words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
     end
   end
 end

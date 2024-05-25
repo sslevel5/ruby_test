d33d848b06f357e3b6d87d9213244b12a76ae212
sslevel5 <sslevel5@gmail.com>
2024-05-25 12:35:25 +0900

    11.4.2

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -5,7 +5,18 @@ require 'json'
 module LogFormatter
   def self.format_log
     uri = URI.parse('https://samples.jnito.com/access-log.json')
-    json = Net::HTTP.get( uri )
-    log_data = JSON.parse( json, symbolize_names: true )
+    json = Net::HTTP.get(uri)
+    log_data = JSON.parse(json, symbolize_names: true)
+    # pp log_data
+    # text = "abc\ndef\n"
+    # text.lines
+    # text.lines(chomp: true)
+
+    log_data.map do |log|
+      case log
+      in {request_id:, path:}
+        "[OK] request_id=#{request_id}, path=#{path}"
+      end
+    end.join("\n")
   end
 end
--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -1,8 +1,11 @@
-require 'minitest'
+require 'minitest/autorun'
 require_relative '../lib/log_formatter'
 
-class LogFormatter < Minitest::Test
+class LogFormatterTest < Minitest::Test
   def test_format_log
-    LogFormatter.format_log
+    # LogFormatter.format_log
+    text = LogFormatter.format_log
+    lines = text.lines(chomp: true)
+    assert_equal '[OK] request_id = 1, path=/products/1', lines[0]
   end
 end

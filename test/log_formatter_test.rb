fea80a964ac914d707e12f36cf88e405fe8621cf
sslevel5 <sslevel5@gmail.com>
2024-05-25 12:49:09 +0900

    11.4.3

--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -6,6 +6,7 @@ class LogFormatterTest < Minitest::Test
     # LogFormatter.format_log
     text = LogFormatter.format_log
     lines = text.lines(chomp: true)
-    assert_equal '[OK] request_id = 1, path=/products/1', lines[0]
+    assert_equal '[OK] request_id=1, path=/products/1', lines[0]
+    assert_equal '[ERROR] request_id = 2, path=/wp-login.php, status = 404, error=Not found', lines[1]
   end
 end
